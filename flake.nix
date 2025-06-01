39ee49f399ed0f3907c669e1c60ed40679c76156
Mathieu Moalic <matmoa@pm.me>
2025-06-01 17:03:37 +0200

    Deduplicate prisma env vars

--- flake.nix
+++ flake.nix
@@ -4,37 +4,43 @@
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+    lib = pkgs.lib;
 
-    package = pkgs.buildNpmPackage {
-      pname = "pleustradenn";
-      version = "2.0.0";
-      npmDepsHash = "sha256-qXEb92J1HLkPC6s7zwF+/Z/K1uUGNL8Bha08o8mVd+0=";
-      src = ./.;
-
+    prismaEnv = {
       PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
       PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
       PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
       PRISMA_HIDE_UPDATE_MESSAGE = "1";
-
-      postBuild = ''
-        export HOME=$TMPDIR
-        npx prisma generate --no-hints
-      '';
-
-      installPhase = ''
-        runHook preInstall
-        mkdir -p $out/bin
-        cp -r build prisma package.json node_modules $out/
-        runHook postInstall
-      '';
-      NODE_ENV = "production";
     };
 
+    exportPrismaEnv = lib.concatStringsSep "\n" (
+      map (name: "export ${name}=\"${prismaEnv.${name}}\"")
+      (builtins.attrNames prismaEnv)
+    );
+
+    package = pkgs.buildNpmPackage ({
+        pname = "pleustradenn";
+        version = "2.0.0";
+        npmDepsHash = "sha256-qXEb92J1HLkPC6s7zwF+/Z/K1uUGNL8Bha08o8mVd+0=";
+        src = ./.;
+        NODE_ENV = "production";
+
+        postBuild = ''
+          export HOME=$TMPDIR
+          npx prisma generate --no-hints
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/bin
+          cp -r build prisma package.json node_modules $out/
+          runHook postInstall
+        '';
+      }
+      // prismaEnv);
+
     startScript = pkgs.writeShellScriptBin "pleustradenn" ''
-      export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
-      export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-      export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-      export PRISMA_HIDE_UPDATE_MESSAGE=1
+      ${exportPrismaEnv}
 
       if [ -f .env ]; then
         exec ${pkgs.nodejs}/bin/node --env-file=.env ${package}/build/index.js "$@"
@@ -50,12 +56,7 @@
 
     shell = pkgs.mkShell {
       buildInputs = with pkgs; [nodejs sqlite];
-      shellHook = ''
-        export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
-        export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-        export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-        export PRISMA_HIDE_UPDATE_MESSAGE=1
-      '';
+      shellHook = exportPrismaEnv;
     };
 
     service = {
@@ -64,110 +65,105 @@
       pkgs,
       utils,
       ...
-    }:
-      with lib; let
-        cfg = config.services.pleustradenn;
-      in {
-        options.services.pleustradenn = {
-          enable = mkEnableOption "Pleustradenn web application";
+    }: let
+      cfg = config.services.pleustradenn;
+    in {
+      options.services.pleustradenn = {
+        enable = lib.mkEnableOption "Pleustradenn web application";
 
-          databaseUrl = mkOption {
-            type = types.str;
-            default = "file:/var/lib/pleustradenn/data.db";
-            description = "Database connection string.";
-          };
-
-          allowRegistration = mkOption {
-            type = types.bool;
-            default = true;
-            description = "Allow users to self-register.";
-          };
-
-          port = mkOption {
-            type = types.port;
-            default = 4173;
-            description = "Port the server listens on.";
-          };
+        databaseUrl = lib.mkOption {
+          type = lib.types.str;
+          default = "file:/var/lib/pleustradenn/data.db";
+          description = "Database connection string.";
         };
 
-        config = mkIf cfg.enable {
-          users.users.pleustradenn = {
-            isSystemUser = true;
-            group = "pleustradenn";
-            home = "/var/lib/pleustradenn";
-            createHome = true;
-          };
-          users.groups.pleustradenn = {};
+        allowRegistration = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Allow users to self-register.";
+        };
 
-          systemd.services.pleustradenn = {
-            description = "Pleustradenn web application";
-            after = ["network.target"];
-            wantedBy = ["multi-user.target"];
+        port = lib.mkOption {
+          type = lib.types.port;
+          default = 4173;
+          description = "Port the server listens on.";
+        };
+      };
 
-            environment = {
+      config = lib.mkIf cfg.enable {
+        users.users.pleustradenn = {
+          isSystemUser = true;
+          group = "pleustradenn";
+          home = "/var/lib/pleustradenn";
+          createHome = true;
+        };
+        users.groups.pleustradenn = {};
+
+        systemd.services.pleustradenn = {
+          description = "Pleustradenn web application";
+          after = ["network.target"];
+          wantedBy = ["multi-user.target"];
+
+          environment =
+            prismaEnv
+            // {
               DATABASE_URL = cfg.databaseUrl;
-              ALLOW_REGISTRATION = boolToString cfg.allowRegistration;
-              PORT = toString cfg.port;
-              PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-              PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-              PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+              ALLOW_REGISTRATION = lib.boolToString cfg.allowRegistration;
+              PORT = builtins.toString cfg.port;
             };
 
-            serviceConfig = {
-              ExecStartPre = pkgs.writeShellScript "run-prisma-db-push" ''
-                export DATABASE_URL="${cfg.databaseUrl}"
-                export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
-                export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-                export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-                export PRISMA_HIDE_UPDATE_MESSAGE=1
+          serviceConfig = {
+            ExecStartPre = pkgs.writeShellScript "run-prisma-db-push" ''
+              export DATABASE_URL="${cfg.databaseUrl}"
+              ${exportPrismaEnv}
 
-                ${package}/node_modules/.bin/prisma db push --accept-data-loss --schema ${package}/prisma/schema.prisma
-              '';
+              ${package}/node_modules/.bin/prisma db push --accept-data-loss --schema ${package}/prisma/schema.prisma
+            '';
 
-              ExecStart = utils.escapeSystemdExecArgs [
-                "${pkgs.nodejs}/bin/node"
-                "${package}/build/index.js"
-              ];
-              WorkingDirectory = "/var/lib/pleustradenn";
-              User = "pleustradenn";
-              Group = "pleustradenn";
-              StateDirectory = "pleustradenn";
+            ExecStart = utils.escapeSystemdExecArgs [
+              "${pkgs.nodejs}/bin/node"
+              "${package}/build/index.js"
+            ];
+            WorkingDirectory = "/var/lib/pleustradenn";
+            User = "pleustradenn";
+            Group = "pleustradenn";
+            StateDirectory = "pleustradenn";
 
-              # These are the security settings for the service
-              CapabilityBoundingSet = "";
-              RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6";
-              SystemCallFilter = "~@clock @cpu-emulation @keyring @module @obsolete @raw-io @reboot @swap @resources @privileged @mount @debug";
-              NoNewPrivileges = "yes";
-              ProtectClock = "yes";
-              ProtectKernelLogs = "yes";
-              ProtectControlGroups = "yes";
-              ProtectKernelModules = "yes";
-              SystemCallArchitectures = "native";
-              RestrictNamespaces = "yes";
-              RestrictSUIDSGID = "yes";
-              ProtectHostname = "yes";
-              ProtectKernelTunables = "yes";
-              RestrictRealtime = "yes";
-              ProtectProc = "invisible";
-              PrivateUsers = "yes";
-              LockPersonality = "yes";
-              UMask = "0077";
-              RemoveIPC = "yes";
-              LimitCORE = "0";
-              ProtectHome = "yes";
-              PrivateTmp = "yes";
-              ProtectSystem = "strict";
-              ProcSubset = "pid";
-              SocketBindAllow = ["tcp:${toString cfg.port}"];
-              SocketBindDeny = "any";
+            # Security hardening
+            CapabilityBoundingSet = "";
+            RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6";
+            SystemCallFilter = "~@clock @cpu-emulation @keyring @module @obsolete @raw-io @reboot @swap @resources @privileged @mount @debug";
+            NoNewPrivileges = "yes";
+            ProtectClock = "yes";
+            ProtectKernelLogs = "yes";
+            ProtectControlGroups = "yes";
+            ProtectKernelModules = "yes";
+            SystemCallArchitectures = "native";
+            RestrictNamespaces = "yes";
+            RestrictSUIDSGID = "yes";
+            ProtectHostname = "yes";
+            ProtectKernelTunables = "yes";
+            RestrictRealtime = "yes";
+            ProtectProc = "invisible";
+            PrivateUsers = "yes";
+            LockPersonality = "yes";
+            UMask = "0077";
+            RemoveIPC = "yes";
+            LimitCORE = "0";
+            ProtectHome = "yes";
+            PrivateTmp = "yes";
+            ProtectSystem = "strict";
+            ProcSubset = "pid";
+            SocketBindAllow = ["tcp:${builtins.toString cfg.port}"];
+            SocketBindDeny = "any";
 
-              LimitNOFILE = 1024;
-              LimitNPROC = 64;
-              MemoryMax = "100M";
-            };
+            LimitNOFILE = 1024;
+            LimitNPROC = 64;
+            MemoryMax = "100M";
           };
         };
       };
+    };
 
     app = {
       type = "app";
