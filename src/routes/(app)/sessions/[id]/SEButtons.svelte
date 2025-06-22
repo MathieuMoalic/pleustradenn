0dc3c81857b2f8da9411119bbba95ffbc0aa8ae5
Mathieu Moalic <matmoa@pm.me>
2025-06-22 21:11:05 +0200

    Refactor SEButtons component: enhance button functionality and improve menu toggle handling

--- src/routes/(app)/sessions/[id]/SEButtons.svelte
+++ src/routes/(app)/sessions/[id]/SEButtons.svelte
@@ -10,8 +10,10 @@
 
     import { confirmable } from "$lib/client/confirm";
     import { exerciseNamei18n } from "$lib/stores/i18n";
+    import Hamburger from "$components/icons/Hamburger.svelte";
+
     let formEl: HTMLFormElement;
-    let deleteBtn: HTMLButtonElement; // the real submitter
+    let deleteBtn: HTMLButtonElement;
 
     export let SE: {
         exercise: {
@@ -29,83 +31,115 @@
         sets: { id: number }[];
     };
 
-    let className = "h-3 w-3";
-    let buttonClass =
-        "p-1.5 rounded-md shadow-xl transition duration-200 bg-seal-brown text-thistle";
-    $: buttonClass = SE.completed
-        ? "p-1.5 rounded-md shadow-xl transition duration-200 bg-emerald-800 text-thistle"
-        : "p-1.5 rounded-md shadow-xl transition duration-200 bg-seal-brown text-thistle";
+    let showMenu = false;
+
+    let svgIconClass = "h-4 w-4";
+    const baseButtonClass =
+        "p-1 rounded-md shadow-xl transition duration-200 text-thistle";
+
+    $: buttonClass = `${baseButtonClass} ${SE.completed ? "bg-emerald-800" : "bg-seal-brown"}`;
 </script>
 
 <div class="px-1 ml-1 py-1 bg-white/5 rounded-r-md flex flex-col gap-1">
-    <form method="POST" action="?/create_set" use:enhance>
-        <input type="hidden" name="session_id" value={SE.session_id} />
-        <input type="hidden" name="exercise_id" value={SE.exercise.id || ""} />
-        <input type="hidden" name="session_exercise_id" value={SE.id || ""} />
-        <button type="submit" class={buttonClass} aria-label="Add set">
-            <Add {className} />
-        </button>
-    </form>
-
-    {#if !(SE.sets[0].id <= 0)}
-        <form method="POST" action="?/update_session_exercise" use:enhance>
-            <input type="hidden" name="id" value={SE.id} />
-            <input type="hidden" name="completed" value={SE.completed} />
-
-            <button
-                type="submit"
-                class={buttonClass}
-                aria-label={SE.completed
-                    ? "Mark as incomplete"
-                    : "Mark as complete"}
-                on:click={() => (SE.completed = !SE.completed)}
-            >
-                {#if SE.completed}
-                    <TickedBox {className} />
-                {:else}
-                    <UntickedBox {className} />
-                {/if}
-            </button>
-        </form>
-    {/if}
-
+    <!-- Toggle Button -->
     <button
         type="button"
-        on:click={() => goto(`/exercises/${SE.exercise.id}`)}
         class={buttonClass}
-        aria-label="Go to log"
+        aria-label="Toggle menu"
+        on:click={() => (showMenu = !showMenu)}
     >
-        <Log {className} />
+        <Hamburger className={svgIconClass} />
     </button>
 
-    <form
-        method="POST"
-        action="?/delete_session_exercise"
-        use:enhance
-        bind:this={formEl}
-    >
-        <!-- hidden real submit button -->
-        <button
-            bind:this={deleteBtn}
-            type="submit"
-            formaction="?/delete_session_exercise"
-            formmethod="POST"
-            class="hidden"
-            aria-hidden="true"
-            name="id"
-            value={SE.id}
-        >
-        </button>
-        <button
-            type="button"
-            use:confirmable={{
-                message: `Are you sure you want to delete '${exerciseNamei18n(SE.exercise)}'?`,
-                onConfirm: () => formEl.requestSubmit(deleteBtn),
-            }}
-            class={buttonClass}
-            aria-label="Add set"
-        >
-            <Bin {className} />
-        </button>
-    </form>
+    {#if showMenu}
+        <div class="flex flex-col gap-1 mt-1">
+            <!-- Add Set Button -->
+            <form method="POST" action="?/create_set" use:enhance>
+                <input type="hidden" name="session_id" value={SE.session_id} />
+                <input
+                    type="hidden"
+                    name="exercise_id"
+                    value={SE.exercise.id || ""}
+                />
+                <input
+                    type="hidden"
+                    name="session_exercise_id"
+                    value={SE.id || ""}
+                />
+                <button type="submit" class={buttonClass} aria-label="Add set">
+                    <Add className={svgIconClass} />
+                </button>
+            </form>
+
+            <!-- Mark Complete/Incomplete -->
+            {#if !(SE.sets[0].id <= 0)}
+                <form
+                    method="POST"
+                    action="?/update_session_exercise"
+                    use:enhance
+                >
+                    <input type="hidden" name="id" value={SE.id} />
+                    <input
+                        type="hidden"
+                        name="completed"
+                        value={SE.completed}
+                    />
+                    <button
+                        type="submit"
+                        class={buttonClass}
+                        aria-label={SE.completed
+                            ? "Mark as incomplete"
+                            : "Mark as complete"}
+                        on:click={() => (SE.completed = !SE.completed)}
+                    >
+                        {#if SE.completed}
+                            <TickedBox className={svgIconClass} />
+                        {:else}
+                            <UntickedBox className={svgIconClass} />
+                        {/if}
+                    </button>
+                </form>
+            {/if}
+
+            <!-- Log Button -->
+            <button
+                type="button"
+                on:click={() => goto(`/exercises/${SE.exercise.id}`)}
+                class={buttonClass}
+                aria-label="Go to log"
+            >
+                <Log className={svgIconClass} />
+            </button>
+
+            <!-- Delete Button -->
+            <form
+                method="POST"
+                action="?/delete_session_exercise"
+                use:enhance
+                bind:this={formEl}
+            >
+                <button
+                    bind:this={deleteBtn}
+                    type="submit"
+                    formaction="?/delete_session_exercise"
+                    formmethod="POST"
+                    class="hidden"
+                    aria-hidden="true"
+                    name="id"
+                    value={SE.id}
+                ></button>
+                <button
+                    type="button"
+                    use:confirmable={{
+                        message: `Are you sure you want to delete '${exerciseNamei18n(SE.exercise)}'?`,
+                        onConfirm: () => formEl.requestSubmit(deleteBtn),
+                    }}
+                    class={buttonClass}
+                    aria-label="Delete exercise"
+                >
+                    <Bin className={svgIconClass} />
+                </button>
+            </form>
+        </div>
+    {/if}
 </div>
